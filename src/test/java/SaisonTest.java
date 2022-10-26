362eb5a652a9391a8a1d42c9902b7e5545ec261f
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 14:38:41 +0200

    Test Unitaire Ex1

---
+++ src/test/java/SaisonTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import fr.diginamic.enumerations.Saison;
+
+public class SaisonTest {
+
+	@Test
+	public void testValueOfLibelle() {
+		assertEquals("La saison avec le libéllé 'Printemps' est bien PRINTEMPS", Saison.PRINTEMPS,
+				Saison.valueOfLibelle("Printemps"));
+
+		assertEquals("La saison avec le libéllé 'Eté' est bien ETE", Saison.ETE, Saison.valueOfLibelle("Eté"));
+
+		assertEquals("La saison avec le libéllé 'Automne' est bien AUTOMNE", Saison.AUTOMNE,
+				Saison.valueOfLibelle("Automne"));
+
+		assertEquals("La saison avec le libéllé 'Hiver' est bien HIVER", Saison.HIVER, Saison.valueOfLibelle("Hiver"));
+
+		assertNull("Il n'y a pas de saison avec le libellé 'hiver'", Saison.valueOfLibelle("hiver"));
+		
+		assertNull("Il n'y a pas de saison avec le libellé '5'", Saison.valueOfLibelle("5"));
+	}
+
+}
